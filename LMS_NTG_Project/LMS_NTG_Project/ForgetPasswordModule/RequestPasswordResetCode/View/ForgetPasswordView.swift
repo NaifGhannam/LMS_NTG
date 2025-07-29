6651706980f4f04aadc0a967984087d33910df93
Tahani50 <tahanyayman4@gmail.com>
2025-07-29 13:19:36 +0300

    feature: Forget Password UI

--- LMS_NTG_Project/LMS_NTG_Project/ForgetPasswordModule/RequestPasswordResetCode/View/ForgetPasswordView.swift
+++ LMS_NTG_Project/LMS_NTG_Project/ForgetPasswordModule/RequestPasswordResetCode/View/ForgetPasswordView.swift
@@ -9,12 +9,32 @@ import SwiftUI
 
 struct ForgetPasswordView: View {
     var body: some View {
+        
         NavigationStack {
-            VStack {
-                HeaderView(showFirstIcon: true, title: "Forgot Password")
-                    
+            
+            HeaderView(showFirstIcon: true, title: "Forgot Password")
+            
+            VStack(spacing: 15) {
+                 
+                Spacer()
+                    .frame(height: UIScreen.main.bounds.size.height * 0.18)
+               
+                Text("Mail Address Here")
+                    .font(.system(size: 22))
+                
+                Text("Enter the email address associated \nwith your account")
+                    .font(.system(size: 16))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black.opacity(0.55))
+                
+                CustomTextField(labelText: "Email", iconName: "mdi_email", placeholder: "Enter your email", text: .constant(""))
+                    .padding(.bottom, 10)
+                
+                CustomButton(title: "Recover Password")
+                
                 Spacer()
             }
+            .padding(.horizontal, 40)
         }
     }
 }
